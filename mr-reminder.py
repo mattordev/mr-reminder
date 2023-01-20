9a10edddcb0cbe0380c751338fb979a16d72d72a
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-01-20 20:48:40 +0000

    feat: added ability to send reminder message and new commands
    added /stopreminder and /listreminders but they currently don't function, still need more work
    renamed a few variable names and fixed a few smaller things

--- mr-reminder.py
+++ mr-reminder.py
@@ -18,6 +18,7 @@ class bot_client(discord.Client):
 client = bot_client()
 tree = app_commands.CommandTree(client)
 
+#commands
 @tree.command(name = "areyouup", description = "Run a simple command to see if the bot is up and running", guild = discord.Object(id = 1065734665333375027))
 async def areyouup(interaction: discord.Interaction):
     await interaction.response.send_message("Alive and waiting...", ephemeral = True)
@@ -29,7 +30,7 @@ async def areyouup(interaction: discord.Interaction):
     await interaction.response.send_message("DM Sent!", ephemeral = True)
     
 @tree.command(name = "reminder", description = "Reminds the user of something after a set amount of time", guild = discord.Object(id = 1065734665333375027))
-async def reminder(interaction: discord.Interaction, interval: float):
+async def reminder(interaction: discord.Interaction, reminder: str, time: float):
     await interaction.response.send_message("Executing...", ephemeral = True)
     # Get the current timestamp 
     current_timestamp = datetime.utcnow().timestamp()
@@ -38,19 +39,19 @@ async def reminder(interaction: discord.Interaction, interval: float):
     current_time = datetime.utcfromtimestamp(current_timestamp)
 
     # Add the interval
-    current_time += timedelta(hours=interval)
+    current_time += timedelta(hours=time)
     # Get the Unix timestamp
     unix_time = int(current_time.timestamp())
     
-    await interaction.edit_original_response(content=f"Reminder setup, reminding <t:{unix_time}:R>")
-    timeInSeconds = interval * 3600
+    await interaction.edit_original_response(content=f"Reminder setup, reminding you <t:{unix_time}:R>")
+    timeInSeconds = time * 3600
     await asyncio.sleep(timeInSeconds)
     channel = await interaction.user.create_dm()
-    await channel.send("REMINDER")
-    await interaction.edit_original_response(content="DM Sent!")
+    await channel.send(f"This is your reminder: {reminder}")
+    await interaction.edit_original_response(content="Reminder sent!")
     
 @tree.command(name = "recurringreminder", description = "Reminds the user of something after a set amount of time, but recurs every interval", guild = discord.Object(id = 1065734665333375027))
-async def recurringreminder(interaction: discord.Interaction, interval: float):
+async def recurringreminder(interaction: discord.Interaction, reminder: str, interval: float):
     await interaction.response.send_message("Executing...", ephemeral = True)
     # Get the current timestamp 
     current_timestamp = datetime.utcnow().timestamp()
@@ -63,12 +64,25 @@ async def recurringreminder(interaction: discord.Interaction, interval: float):
     # Get the Unix timestamp
     unix_time = int(current_time.timestamp())
     
-    await interaction.edit_original_response(content=f"Reminder setup, reminding you every {interval}, the next reminder is <t:{unix_time}:R>")
+    await interaction.edit_original_response(content=f"Reminder setup, reminding you every {interval}h, the next reminder is <t:{unix_time}:R>")
+    # Get the interval in seconds so we can use it for the asyncio sleep call.
     timeInSeconds = interval * 3600
+    
     await asyncio.sleep(timeInSeconds)
     channel = await interaction.user.create_dm()
-    await channel.send("REMINDER")
-    await interaction.edit_original_response(content="DM Sent!")
+    await channel.send(f"This is your reminder: {reminder}")
+    await interaction.edit_original_response(content=f"Reminder sent! Sending another one <t:{unix_time}:R>. Run /stopreminder to cancel.")
+
+        
+@tree.command(name = "stopreminder", description = "Reminds the user of something after a set amount of time, but recurs every interval", guild = discord.Object(id = 1065734665333375027))
+async def stopreminder(interaction: discord.Interaction, remindername: str):
+    await interaction.response.send_message(f"Cancelling your reminder called: {remindername}", ephemeral = True)
+
+@tree.command(name = "listreminders", description = "Lists the currently active reminders", guild = discord.Object(id = 1065734665333375027))
+async def listreminders(interaction: discord.Interaction):
+    await interaction.response.send_message("Getting the list of active reminders...", ephemeral=True)
+    await interaction.edit_original_response(content = f"Here are all of your reminders: <REMINDERS>")
+        
         
 
 token = open('token.txt', 'r').read()
