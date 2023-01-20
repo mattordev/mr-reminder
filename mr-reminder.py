af2d3be1a2bf310d244ed34414ac2cc4552da06f
Matthew Roberts <mr232432@falmouth.ac.uk>
2023-01-20 00:48:15 +0000

    feat: added more functionality to the reminder command

--- mr-reminder.py
+++ mr-reminder.py
@@ -1,4 +1,6 @@
 import discord
+from datetime import datetime, timedelta
+from asyncio import sleep
 from discord import app_commands
 
 class bot_client(discord.Client):
@@ -17,14 +19,29 @@ client = bot_client()
 tree = app_commands.CommandTree(client)
 
 @tree.command(name = "test", description = "Run a simple test command", guild = discord.Object(id = 1065734665333375027))
-async def self(interaction: discord.Interaction, channel: discord.VoiceChannel):
-    await interaction.response.send_message(f"<#{channel.id}>", ephemeral=False)
-    await interaction.edit_original_response(content=f"Invalid Cron Expression")
-    print(f'Current channel: {channel.name}')
+async def test(interaction: discord.Interaction):
+    await reminder(interaction, 5, False)
     
-@tree.command(name = "remindme", description = "Reminds the user of something, after a set amount of time", guild = discord.Object(id = 1065734665333375027))
-async def self(interaction: discord.Interaction, time: str):
-    await interaction.response.send_message(f"I will remind you in {time}", ephemeral=False)
+@tree.command(name = "reminder", description = "Reminds the user of something, after a set amount of time", guild = discord.Object(id = 1065734665333375027))
+async def reminder(interaction: discord.Interaction, interval: int, recurring: bool):
+    await interaction.response.send_message("Executing...", ephemeral = True)
+    # Get the current timestamp 
+    current_timestamp = datetime.utcnow().timestamp()
+
+    # Create a datetime object from the timestamp
+    current_time = datetime.utcfromtimestamp(current_timestamp)
+
+    current_time += timedelta(hours=interval)
+
+    # Get the Unix timestamp
+    unix_time = int(current_time.timestamp())
+    
+    if(recurring == False):
+        await interaction.edit_original_response(content=f"Reminder setup, reminding <t:{unix_time}:R>")
+    else:
+        await interaction.edit_original_response(content=f"Reminder setup, reminding you every {interval}, the next reminder is <t:{unix_time}:R>")
+    
+
 
 token = open('token.txt', 'r').read()
 client.run(token)
